a733009998e4689f220025fa99508e4ec18d5a5f
kentxvalentines <kentyucca@gmail.com>
2022-04-15 15:00:44 +0800

    add firefox

---
+++ firefox-openh264.md
@@ -0,0 +1,11 @@
+ 1. open Firefox, go to menu → Add-ons → Plugins and enable OpenH264 plugin
+ 2. open about:config
+ 3. Set values below
+ ```
+media.gmp-gmpopenh264.autoupdate
+media.gmp-gmpopenh264.enabled
+media.gmp-gmpopenh264.provider.enabled
+media.peerconnection.video.h264_enabled
+ ```
+
+ 4. restart firefox
--- firefox-openh264.sh
+++ firefox-openh264.sh
@@ -1,1 +1,11 @@
- 
+#!/bin/bash
+# Fix Firefox video playback low FPS issue
+
+# Install OpenH264 binaries
+sudo dnf config-manager --set-enabled fedora-cisco-openh264
+
+# Install OpenH264 Firefox plugin
+sudo dnf install gstreamer1-plugin-openh264 mozilla-openh264
+
+# Install ffmpeg libs
+sudo dnf install ffmpeg-libs
